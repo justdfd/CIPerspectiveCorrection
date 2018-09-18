ba5899080ce0a49071444e0c8140bb1cbbbf206a
dfd <dfd@justdfd.com>
2018-09-18 06:02:02 -0400

    Initial commit.

--- CIPerspectiveCorrection/ViewController.swift
+++ CIPerspectiveCorrection/ViewController.swift
@@ -10,8 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let ciOriginal = CIImage(image: UIImage(named: "original.png")!)
+    @IBOutlet weak var imageView: UIImageView!
+    @IBAction func cropImage(_ sender: UIButton) {
+        
+        let inputBottomLeft = CIVector(x: 38, y: 122)
+        let inputTopLeft = CIVector(x: 68, y: 236)
+        let inputTopRight = CIVector(x: 146, y: 231)
+        let inputBottomRight = CIVector(x: 151, y: 96)
+
+        let filter = CIFilter(name: "CIPerspectiveCorrection")
+        filter?.setValue(inputTopLeft, forKey: "inputTopLeft")
+        filter?.setValue(inputTopRight, forKey: "inputTopRight")
+        filter?.setValue(inputBottomLeft, forKey: "inputBottomLeft")
+        filter?.setValue(inputBottomRight, forKey: "inputBottomRight")
+
+        filter?.setValue(ciOriginal, forKey: "inputImage")
+        let ciOutput = filter?.outputImage
+        
+        imageView.image = UIImage(ciImage: ciOutput!)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        imageView.image = UIImage(named: "original.png")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
